9e84c89ab18032b61980ba4f45dde0ecb45677e3
Boshra Jaber <boshrajaber94@gmail.com>
2021-03-29 23:09:57 +0300

    tesing

--- challenges/__tests__/array-shift-test.js
+++ challenges/__tests__/array-shift-test.js
@@ -1,5 +1,5 @@
 const superTest = require('supertest');
-const arrayShift = require('../arrayShift/array-shift').insertShiftArray
+const insertShiftArray = require('../arrayShift/array-shift').insertShiftArray
 
 describe('Testing array shift', () => {
     test('It should add the value in the middle index', () => {
