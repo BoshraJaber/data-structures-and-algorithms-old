3286ab457d63ab6d046e498b26d530d4cd7e77db
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-02 17:50:08 +0300

    testing edge cases

--- challenges/__tests__/array-binary-search-test.js
+++ challenges/__tests__/array-binary-search-test.js
@@ -1,11 +1,13 @@
 const superTest = require('supertest');
 const binarySearch = require('../arrayBinarySearch/array-binary-search').binarySearch
 
-describe('Testing array shift', () => {
-    test('It should add the value in the middle index', () => {
+describe('Testing array binary search', () => {
+    test('It should find the index of key value using binary search', () => {
         expect(binarySearch([2, 4, 6, 8], 4)).toStrictEqual(1);
         expect(binarySearch([4, 8, 15, 16, 42], 16)).toStrictEqual(3);
         expect(binarySearch([], 16)).toStrictEqual(-1);
-        expect(binarySearch([5,6,7,8], )).toStrictEqual(-1);
+        expect(binarySearch([5,6,7,8],0)).toStrictEqual(-1);
+        expect(binarySearch([-1,3,4,7],-1)).toStrictEqual(0);
+
     });
 });
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,28 +1,29 @@
 'use strict';
-
 const binarySearch = (sortedArr, searchKey) => {
-    let startIndex = 0;
-    let endIndex = sortedArr.length - 1;
-    
-    while(startIndex <= endIndex) {
-      let middleIndex = Math.floor((startIndex + endIndex) / 2);
-      // Checking if the middle element equal the search key
-      if(searchKey=== sortedArr[middleIndex]) {
-        return  middleIndex;
-      }
-      // Checking if the search key in the right half
-      if(searchKey > sortedArr[middleIndex]) {
-        startIndex = middleIndex + 1;
-      }
-      // Checking if the search key in the left half
-      if(searchKey < sortedArr[middleIndex]) {
-        endIndex = middleIndex - 1;
-      }
+  let startIndex = 0;
+  let endIndex = sortedArr.length - 1;
+  
+  while(startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    // Checking if the middle element equal the search key
+    if(searchKey=== sortedArr[middleIndex]) {
+      return  middleIndex;
     }
-    return -1;
+    // Checking if the search key in the right half
+    if(searchKey > sortedArr[middleIndex]) {
+      startIndex = middleIndex + 1;
+    }
+    // Checking if the search key in the left half
+    if(searchKey < sortedArr[middleIndex]) {
+      endIndex = middleIndex - 1;
+    }
+  }
+  return -1;
 }
 
-  binarySearch([1, 2, 3, 5, 6, 7], 7)
+binarySearch([1, 2, 3, 5, 6, 7], 7)
+
+binarySearch([1, 2, 3, 5, 6, 7], 7)
 
 binarySearch([1, 2, 3, 5, 6, 7], 4);
 
