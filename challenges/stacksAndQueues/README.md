666a57a23b8a9aa032985b003f66e394257b2bb7
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-11 19:33:13 +0300

    stack test passed

---
+++ challenges/stacksAndQueues/README.md
@@ -0,0 +1,4 @@
+## Resources:
+[Reference1](https://www.javatpoint.com/ds-linked-list-implementation-of-stack)
+[Reference2](https://osgoodgunawan.medium.com/stack-and-queue-in-linkedlist-javascript-76ca86dd95fe)
+[Reference3](https://learnersbucket.com/tutorials/data-structures/implement-queue-using-linked-list/)
---
+++ challenges/stacksAndQueues/stacks-and-queues.js
@@ -0,0 +1,104 @@
+'use strict';
+//Node class that has properties for the value stored in the Node, and a pointer to the next node.
+class Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = null;
+    }
+}
+//LIFO: Last in from the front of the list is the first to get out.
+class Stack {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value) {
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+        } else {
+            let previousHead = this.head;
+            this.head = newNode;
+            this.head.next = previousHead;
+        }
+        this.length++;
+    }
+    pop() {
+        if(!this.head){
+            return 'exception';
+        }
+        let removedNode = this.head;
+        if(this.head == this.tail){
+            this.tail = null;
+        }
+        this.head = this.head.next;
+        this.length--;
+        return removedNode.value;
+    }
+    peek() {
+        if(!this.head){
+            return 'exception'
+        }
+        let top = this.head.value
+        return top;
+    }
+    isEmpty(){
+        if(!this.head){
+            return true
+        } else {
+            return false;
+        }
+    }
+}
+// Queue Class
+class Queue{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    enqueue(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+        } else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        length++;
+    }
+    dequeue(){
+        if(!this.head){
+            return 'exception';
+        }
+        let removedNode = this.head;
+        if(this.head == this.tail){
+            this.tail = null;
+        }
+        this.head = this.head.next;
+        length--;
+        return removedNode.value;
+    }
+    peek(){
+        if(!this.head){
+            return 'exception';
+        } else {
+            return head.value;
+        }
+    }
+    isEmpty(){
+        if(!this.head){
+            return true
+        } else {
+            return false;
+        }
+    }
+}
+
+
+module.exports = {
+    node : Node,
+    stack : Stack,
+    queue : Queue,
+}
