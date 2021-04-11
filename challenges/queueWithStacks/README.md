870c689a1943063cb1e21480056abae6961a2752
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-11 21:49:21 +0300

    queue with 2 stacks passed

---
+++ challenges/queueWithStacks/README.md
@@ -0,0 +1,5 @@
+
+
+
+## Resources:
+[Reference1](https://medium.com/@larry.sassainsworth/mplementing-a-queue-using-stacks-in-javascript-fe78176ba028)
--- challenges/queueWithStacks/queue-with-stacks.js
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -1,1 +1,28 @@
 'use strict';
+const Stack = require('../stacksAndQueues/stacks-and-queues').stack;
+
+class PseudoQueue{
+    constructor(){
+        this.stackPush = new Stack();
+        this.stackPop = new Stack();
+        this.length = 0;
+    }
+    enqueue(value){
+        // for (let i = 0; i < this.stackPop.length; i++) {
+        //     this.stackPush.push(this.last.pop());
+            
+        // }
+        this.stackPush.push(value);  
+        this.length++;      
+    }
+    dequeue(){
+        while(this.stackPush.length>0){
+            this.stackPop.push( this.stackPush.pop())
+        }
+        this.length--;
+        return this.stackPop.pop();
+
+    }
+}
+
+module.exports = PseudoQueue;
