c3acd611c6a628d92171708d84d58a041896f146
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-16 00:37:11 +0300

    binary tree class added

--- challenges/tree/tree.test.js
+++ challenges/tree/tree.test.js
@@ -1,6 +1,6 @@
 'use strict';
 class Node {
-    constructor(value, left = null, right = null){
+    constructor(value, left = null, right = null) {
         this.value = value;
         this.left = left;
         this.right = right;
@@ -8,65 +8,82 @@ class Node {
 }
 class BinaryTree {
     constructor(root = null) {
-      this.root = root;
+        this.root = root;
     }
-  
     // Time : O(n) n = the number of nodes
     // Space : O(h) h = h is the hight of the tree
     // Root = Left - Right
     preOrder() {
-      const results = [];
-      const _traverse = (node) => {
-        //push the value to the array
-        results.push(node.value);
-        // check if it have left _traverse(node.left)
-        if (node.left) _traverse(node.left);
-        // check if it have right _traverse(node.right)
-        if (node.right) _traverse(node.right);
-      };
-      _traverse(this.root);
-      return results;
+        try {
+            const results = [];
+            const _traverse = (node) => {
+                results.push(node.value);
+                if (node.left) _traverse(node.left);
+                if (node.right) _traverse(node.right);
+            };
+            _traverse(this.root);
+            return results;
+        } catch (error) {
+            throw new Error(`error in traversing using preOrder method ${error}`);
+        }
     }
-  
     // Left - Root - Right
     inOrder() {
-      const results = [];
-      const _traverse = (node) => {
-        // check if it have left _traverse(node.left)
-        if (node.left) _traverse(node.left);
-        //push the value to the array
-        results.push(node.value);
-        // check if it have right _traverse(node.right)
-        if (node.right) _traverse(node.right);
-      };
-      _traverse(this.root);
-      return results;
+        try {
+            const results = [];
+            const _traverse = (node) => {
+                if (node.left) _traverse(node.left);
+                results.push(node.value);
+                if (node.right) _traverse(node.right);
+            };
+            _traverse(this.root);
+            return results;
+
+        } catch (error) {
+            throw new Error(`error in traversing using inOrder method ${error}`);
+        }
+
     }
     // left - Right - Root
     postOrder() {
-      const results = [];
-      const _traverse = (node) => {
-        // check if it have left _traverse(node.left)
-        if (node.left) _traverse(node.left);
-        // check if it have right _traverse(node.right)
-        if (node.right) _traverse(node.right);
-        //push the value to the array
-        results.push(node.value);
-      };
-      _traverse(this.root);
-      return results;
+        try {
+            const results = [];
+            const _traverse = (node) => {
+                if (node.left) _traverse(node.left);
+                if (node.right) _traverse(node.right);
+                results.push(node.value);
+            };
+            _traverse(this.root);
+            return results;
+        } catch (error) {
+            throw new Error(`error in traversing using postOrder method ${error}`);
+        }
     }
-  }
-  
-  module.exports = BinaryTree;
+}
+
+class BinarySearchTree{
+    constructor(root = null){
+        this.root = root
+    }
+    add(value){
+        try{
+
+        }catch(error){
+            throw new Error(`Error in adding a new node to the binary tree ${error}`)
+        }
+    }
+    contains(value){
+        try{
+
+        }catch(error){
+            throw new Error(`Error finding the ${value} in the binary tree ${error}`)
+        }
+    }
+}
+
+module.exports = BinaryTree;
 
 
 
-// Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
-// Create a BinaryTree class
-// Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
-// Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-
-// Create a BinarySearchTree class
 // Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
 // Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
