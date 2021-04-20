c7b8654f1190e7b7dc7201b6bec5edd6e45bacd3
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-21 02:10:12 +0300

    test is done

--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,31 +1,53 @@
 'use strict';
 class Node {
     constructor(value) {
-      this.value = value;
-    //   this.children = children;
+        this.value = value;
+        //   this.children = children;
     }
 }
 class katyTree {
     constructor() {
-      this.root = null;
-      this.children = [];
+        this.root = null;
+        this.children = [];
     }
-}
-
-function FizzBuzzTree(tree){
-    if(tree.root == null) return "K-ary tree is null";
-    for(let i = 0; i < tree.children.length; i++){
-        if(tree.children[i].value % 3 === 0 && tree.children[i].value % 5 === 0){
-          tree.children[i].value = 'fizzbuzz';
-        } else if(tree.children[i].value % 3 === 0) {
-          tree.children[i].value = 'fizz';
-        }  else if(tree.children[i].value % 5 === 0) {
-          tree.children[i].value = 'buzz';
-        }  
+    add(value) {
+       const node = new Node(value);
+        if(this.root === null) {
+          this.root = node;
+        } else {
+          this.children.push(node);
+        }
+        return node;
       }
 }
 
-module.exports ={
+function FizzBuzzTree(tree) {
+    if (tree.root == null) return "K-ary tree is null";
+    // Checking the Root node
+    if (tree.root.value % 3 === 0 && tree.root.value % 5 === 0) {
+        tree.root.value = 'FizzBuzz';
+    } else if (tree.root.value % 3 === 0) {
+        tree.root.value = 'Fizz';
+    } else if (tree.root.value % 5 === 0) {
+        tree.root.value = 'Buzz';
+    } else {
+        tree.root.value = tree.root.value.toString();
+    }
+    for (let i = 0; i < tree.children.length; i++) {
+        if (tree.children[i].value % 3 === 0 && tree.children[i].value % 5 === 0) {
+            tree.children[i].value = 'FizzBuzz';
+        } else if (tree.children[i].value % 3 === 0) {
+            tree.children[i].value = 'Fizz';
+        } else if (tree.children[i].value % 5 === 0) {
+            tree.children[i].value = 'Buzz';
+        } else {
+            tree.children[i].value = tree.children[i].value.toString();
+        }
+    }
+    return tree;
+}
+
+module.exports = {
     Node: Node,
     katyTree: katyTree,
     FizzBuzzTree: FizzBuzzTree,
--- challenges/fizzBuzzTree/fizz-buzz-tree.test.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.test.js
@@ -1,45 +1,49 @@
 'use strict';
 const Node = require('./fizz-buzz-tree').Node;
-const BinaryTree = require('./fizz-buzz-tree').katyTree;
+const katyTree = require('./fizz-buzz-tree').katyTree;
 const FizzBuzzTree = require('./fizz-buzz-tree').FizzBuzzTree;
 describe('Binary Tree', () => {
   let tree;
   beforeAll(() => {
-    const one = new Node(1);
-    const two = new Node(2);
-    const three = new Node(3);
-    const four = new Node(4);
-    const five = new Node(5);
-    const six = new Node(6);
-    const seven = new Node(7);
-    const eight = new Node(8);
-    one.left = two;
-    one.right = three;
-    two.left = four;
-    two.right = five;
-    five.left = eight;
-    three.left = six;
-    three.right = seven;
-    tree = new BinaryTree(one);
+    tree = new katyTree();
+    tree.add(1);
+    tree.add(4);
+    tree.add(5);
+    tree.add(3);
+    tree.add(15);
+    tree.add(30);
+    tree.add(6);
+    console.log(tree);
+    // children= [
+    //     Node { value: 4 },
+    //     Node { value: 5 },
+    //     Node { value: 3 },
+    //     Node { value: 15 },
+    //     Node { value: 30 },
+    //     Node { value: 6 },
+    //   ];
   });
 
-  it('should successfully return a collection from a preOrder traversal', () => {
-    const expected = [1, 2, 4, 5, 8, 3, 6, 7];
-    const preOrder = tree.preOrder();
-    expect(preOrder).toEqual(expected);
+  it('should successfully return a “Fizz” If the value is divisible by 3', () => {
+    const expected = FizzBuzzTree(tree);
+    expect(expected.children[2].value).toEqual('Fizz');
   });
-  it('should successfully return a collection from an inOrder traversal', () => {
-    const expected = [4, 2, 8, 5, 1, 6, 3, 7];
-    const inOrder = tree.inOrder();
-    expect(inOrder).toEqual(expected);
+  it('should successfully return a “Buzz” If the value is divisible by 5', () => {
+    const expected = FizzBuzzTree(tree);
+    expect(expected.children[1].value).toEqual('Buzz');
   });
-  it('should successfully return a collection from a postOrder traversal', () => {
-    const expected = [4, 8, 5, 2, 6, 7, 3, 1];
-    const postOrder = tree.postOrder();
-    expect(postOrder).toEqual(expected);
+  it('should successfully return a “FizzBuzz” If the value is divisible by 3 and 5', () => {
+    const expected = FizzBuzzTree(tree);
+    expect(expected.children[3].value).toEqual('FizzBuzz');
+    expect(expected.children[4].value).toEqual('FizzBuzz');
+  });
+  it('should successfully turn the number to string If the value is not divisible by 3 or 5', () => {
+    const expected = FizzBuzzTree(tree);
+    expect(typeof expected.children[0].value).toEqual('string');
+    expect(typeof expected.children[5].value).toEqual('string');
   });
 });
 
-// Can successfully instantiate an empty tree
-// Can successfully instantiate a tree with a single root node
-// Can successfully add a left child and right child to a single root node
+// If the value is divisible by 5, replace the value with “Buzz”
+// If the value is divisible by 3 and 5, replace the value with “”
+// If the value is not divisible by 3 or 5, simply turn the number into a String.
